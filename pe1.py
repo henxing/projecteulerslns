be9c4bbd4744b0d968bbf03241f725eb4ebd4bdc
Hugh Enxing <henxing@draper.com>
2016-12-15 14:16:38 -0500

    refactoring for reusability

--- pe1.py
+++ pe1.py
@@ -1,7 +1,13 @@
-thesum = 0
+def sumMultiples(max_value):
+    thesum = 0
 
-for i in range(1000):
-    if i%3==0 or i%5==0:
-        thesum += i
+    for i in range(max_value):
+        if i%3==0 or i%5==0:
+            thesum += i
 
-print(thesum) # 233168
+    return thesum
+
+if __name__=="__main__":
+    max_value = 1000
+    thesum = sumMultiples(max_value)
+    print(thesum) # 233168
